53f83b64ceff34587c77df970a6caecbc23821f3
lan <lupamo93@gmail.com>
2018-08-13 19:08:40 +0300

    add folders, file structure

--- portfolioproject/settings.py
+++ portfolioproject/settings.py
@@ -37,6 +37,9 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'portfolioapp',
+    'bootstrap4',
+    'tinymce',
 ]
 
 MIDDLEWARE = [
