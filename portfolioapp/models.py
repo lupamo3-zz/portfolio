4be336c6c4923dd69cb5030635e31a4d9b9ad205
lan <lupamo93@gmail.com>
2018-08-17 14:47:58 +0300

    projects, portfolio changes, models, admin.py

--- portfolioapp/models.py
+++ portfolioapp/models.py
@@ -5,3 +5,10 @@ class Portfolio(models.Model):
     viewmore=models.CharField(max_length=30)
     title=models.CharField(max_length=30)
     view=models.CharField(max_length=25)
+    portfolio=models.ImageField(upload_to = 'image/', blank=True)
+
+    def __str__(self):
+        return self.title
+
+    class Meta:
+        ordering = ['title']
