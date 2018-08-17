00e967bad86c42fa30e595dcae6a8e7dcfd20caa
lan <lupamo93@gmail.com>
2018-08-17 12:56:56 +0300

    projects, portfolio changes, models, admin.py

--- portfolioapp/models.py
+++ portfolioapp/models.py
@@ -1,3 +1,7 @@
 from django.db import models
 
 # Create your models here.
+class Portfolio(models.Model):
+    viewmore=models.CharField(max_length=30)
+    title=models.CharField(max_length=30)
+    view=models.CharField(max_length=25)
